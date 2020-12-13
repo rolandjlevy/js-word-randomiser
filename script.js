8b780898c28ceb82106ce1b5c461b41516eafbe3
Roland Levy <rolandjlevy@gmail.com>
2020-12-13 00:18:08 +0000

    Refactored

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 const maxHex = parseInt('ffffff', 16);
+let timerId;
 
 const abcArr = () => {
   return Array(26).fill().map((_, i) => String.fromCharCode('a'.charCodeAt(0) + i));
 }
 
 const abcUC = abcArr().map(n => n.toUpperCase());
-const abc = [...abcArr(), ...abcUC, ...'____'];
+const abc = [...abcArr(), ...abcUC, ...'____.,:;!@-=+£$%^&*()~'];
 
 const randomHex = () => {
   const num = Math.floor(Math.random() * Math.floor(maxHex));
@@ -26,24 +27,28 @@ $('button[name=randomise]').addEventListener('click', (e) => {
 
 $('input[name=words]').addEventListener('keyup', (e) => {
   const inputText = e.target.value.trim();
-  console.log(inputText.length)
   $('button[name=randomise]').disabled = !inputText.length;
 });
 
 $('input[name=words]').focus();
 
 function shuffle(inputText) {
+  if (timerId) {
+    clearInterval(timerId);
+    $('.normal').innerHTML = '';
+  }
   const destination = Array.from(inputText);
   const current = Array(destination.length).fill('').map(item => {
     return { letter:item, colour:`#${randomHex()}` }
   });
-  const timerId = setInterval(() => {
+  timerId = setInterval(() => {
     $('.normal').innerHTML = current.map((item, index) => {      
       if (item.letter !== destination[index]) {
         item.letter = randomChar();
         item.colour = randomHex();
       }
-      return `<span style="--col:#${item.colour}" class="text">${item.letter}</span>`;
+      const colour = item.letter == '_' ? '#000' : item.colour;
+      return `<span style="--col:#${colour}" class="text">${item.letter}</span>`;
     }).join('');
     $('.mirrored').innerHTML = $('.normal').innerHTML;
     if (current.map(item => item.letter).join('') == destination.join('')) {
