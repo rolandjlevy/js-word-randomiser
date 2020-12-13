cfdd5edffe1ffe8bd64e7013499a25a332d2ee7f
Roland Levy <rolandjlevy@gmail.com>
2020-12-13 17:30:21 +0000

    Added slider input for changing speed

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const maxHex = parseInt('ffffff', 16);
 let timerId;
 
 const abcArr = () => {
-  return Array(26).fill().map((_, i) => String.fromCharCode('a'.charCodeAt(0) + i));
+  return Array(26).fill().map((_, i) => {
+    return String.fromCharCode('a'.charCodeAt(0) + i);
+  });
 }
 
 const abcUC = abcArr().map(n => n.toUpperCase());
@@ -19,6 +21,17 @@ const randomChar = () => {
   const randomNum = Math.floor(Math.random() * (abc.length - 1));
   return abc[randomNum];
 }
+
+$('input[name=slider]').addEventListener('input', (e) => {
+  setSpeed(e.target.value);
+  if (timerId) {
+    clearInterval(timerId);
+  }
+});
+
+const setSpeed = (speed) => $('.slider-display').textContent = 'Speed: ' + speed;
+
+setSpeed(50);
 
 $('button[name=randomise]').addEventListener('click', (e) => {
   shuffle();
@@ -38,24 +51,26 @@ function shuffle() {
   if (timerId) {
     clearInterval(timerId);
     $('.normal').innerHTML = '';
+    $('.mirrored').innerHTML = '';
   }
+  const speed = $('.slider').value;
   const inputText = $('input[name=words]').value.trim().replace(/ /g, "_")
   const destination = Array.from(inputText);
   const current = Array(destination.length).fill('').map(item => {
     return { letter:item, colour:`#${randomHex()}` }
   });
   timerId = setInterval(() => {
-    $('.normal').innerHTML = current.map((item, index) => {      
+    $('.normal').innerHTML = current.map((item, index) => {
       if (item.letter !== destination[index]) {
         item.letter = randomChar();
         item.colour = randomHex();
       }
-      const colour = item.letter == '_' ? '#000' : item.colour;
+      const colour = item.letter == '_' ? '000' : item.colour;
       return `<span style="--col:#${colour}" class="text">${item.letter}</span>`;
     }).join('');
     $('.mirrored').innerHTML = $('.normal').innerHTML;
     if (current.map(item => item.letter).join('') == destination.join('')) {
       clearInterval(timerId);
     }
-  }, 50);
+  }, speed);
 }
